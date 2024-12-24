3bac7432c779e9b6b9a4b7c463ca0c37f9bd9780
Smorodin Sergey <ssmorodin@bk.ru>
2024-12-25 00:47:03 +0300

    first_project

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,35 +1,56 @@
+import datetime
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from django.http import JsonResponse
 
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+
     context = {
         'pages': pages
     }
+
     return render(request, template_name, context)
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    template_name = 'app/time.html'
+
+    current_time = datetime.datetime.now().time()
+    formatted_time = current_time.strftime("%H:%M:%S")
+
+    context = {
+        'time': formatted_time
+    }
+
+    return render(request, template_name, context)
+
+
+def get_time(request):
+    current_time = datetime.datetime.now().time()
+    formatted_time = current_time.strftime("%H:%M:%S")
+
+    return JsonResponse({'time': formatted_time})
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    template_name = 'app/workdir.html'
+
+    current_directory = os.getcwd()  # получение пути к рабочему каталогу
+    files = os.listdir(current_directory)  # возвращает список всех файлов
+    files = [f for f in files if os.path.isfile(os.path.join(current_directory, f))]  # исключаем папки
+
+    context = {
+        'files': files
+    }
+
+    return render(request, template_name, context)
